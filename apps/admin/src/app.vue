cb5add0f4eee58f8ce353b35f5115e0693723e3e
j10c <blyb1739@gmail.com>
2025-10-19 14:14:57 +0800

    feat(admin): initilize vant component library

--- apps/admin/src/app.vue
+++ apps/admin/src/app.vue
@@ -1,3 +1,4 @@
 <template>
   <h1>Admin</h1>
+  <van-button>click</van-button>
 </template>
--- apps/admin/vite.config.ts
+++ apps/admin/vite.config.ts
@@ -1,7 +1,18 @@
+import { VantResolver } from "@vant/auto-import-resolver";
 import vue from "@vitejs/plugin-vue";
+import AutoImport from "unplugin-auto-import/vite";
+import Components from "unplugin-vue-components/vite";
 import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [
+    vue(),
+    AutoImport({
+      resolvers: [VantResolver()]
+    }),
+    Components({
+      resolvers: [VantResolver()]
+    })
+  ]
 });
