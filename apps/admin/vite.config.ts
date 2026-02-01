2d47a56a232d1aefa359572d5b4298001edd6cd8
DumbDaiDai <3234627960@qq.com>
2026-02-02 00:44:13 +0800

    chore: 服务端地址环境变量与proxy代理

--- apps/admin/vite.config.ts
+++ apps/admin/vite.config.ts
@@ -3,24 +3,35 @@ import vue from "@vitejs/plugin-vue";
 import path from "path";
 import AutoImport from "unplugin-auto-import/vite";
 import Components from "unplugin-vue-components/vite";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    AutoImport({
-      resolvers: [VantResolver()],
-      dts: "types/auto-imports.d.ts"
-    }),
-    Components({
-      resolvers: [VantResolver()],
-      dts: "types/components.d.ts"
-    })
-  ],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src")
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+  return {
+    plugins: [
+      vue(),
+      AutoImport({
+        resolvers: [VantResolver()],
+        dts: "types/auto-imports.d.ts"
+      }),
+      Components({
+        resolvers: [VantResolver()],
+        dts: "types/components.d.ts"
+      })
+    ],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src")
+      }
+    },
+    server: {
+      proxy: {
+        "/api": {
+          target: env.VITE_HOST,
+          changeOrigin: true
+        }
+      }
     }
-  }
+  };
 });
