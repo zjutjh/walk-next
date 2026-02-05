69646907db7202342473dfc36faedfc54fde6f83
j10c <blyb1739@gmail.com>
2026-02-05 23:37:10 +0800

    refactor(px2rem): 优化代码结构

--- apps/admin/src/configs/px-to-rem.ts
+++ apps/admin/src/configs/px-to-rem.ts
@@ -8,11 +8,11 @@ const MAX_VIEW_WIDTH = 750;
 /**
  * 获取视口宽度，限制了最大视口宽度，防止在超大屏幕上字体过大
  */
-function getViewWidth() {
+export function getViewWidth() {
   return Math.min(document.documentElement.clientWidth || window.innerWidth, MAX_VIEW_WIDTH);
 }
 
-function setRootFontSize() {
+export function setRootFontSize() {
   const size = (getViewWidth() / DESIGN_WIDTH) * BASE_ROOT_FONT_SIZE;
   document.documentElement.style.fontSize = `${size}px`;
 }
@@ -25,26 +25,8 @@ export function initializeRootFontSize() {
   window.addEventListener("resize", setRootFontSize);
   window.addEventListener("pageshow", (e) => {
     if (e.persisted) {
-      // 处理机型返回缓存
+      // 部分手机浏览器执行返回操作时，会从缓存中恢复页面，此时也设置字体
       setRootFontSize();
     }
   });
-}
-
-/**
- * 手动将 px 转换为 rem，适用于动态计算元素尺寸时使用
- */
-export function pxToRem(px: number): `${number}rem` {
-  return `${(px / DESIGN_WIDTH) * BASE_ROOT_FONT_SIZE}rem`;
-}
-
-/**
- * 手动将 px 缩放为当前视口宽度下合适的 px 尺寸
- *
- * 优先使用 `pxToRem`，如果场景不允许消费 rem 单位的尺寸，就使用这个
- *
- * 计算出的尺寸不会随屏幕宽度变化有响应式关系
- */
-export function pxToSize(px: number): number {
-  return (px / DESIGN_WIDTH) * getViewWidth();
 }
