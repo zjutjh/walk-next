ea9a3d9af0db511d4e512b3b6d5ff5fe78805363
maojianhui <maojianhui2006@gmail.com>
2026-02-09 16:47:21 +0800

    fix: color config

--- apps/admin/src/app.vue
+++ apps/admin/src/app.vue
@@ -1,3 +1,22 @@
 <template>
-  <router-view />
+  <van-config-provider :theme-vars="themeVars" theme-vars-scope="global">
+    <router-view />
+  </van-config-provider>
 </template>
+
+<script setup lang="ts">
+const themeVars = {
+  primaryColor: "var(--palette-pink)",
+  successColor: "var(--palette-success)",
+  warningColor: "var(--palette-waiting)",
+  dangerColor: "var(--palette-coral)",
+  infoColor: "var(--palette-info)",
+  textColor: "var(--palette-black)",
+  textColor2: "var(--palette-text-secondary)",
+  textColor3: "var(--palette-text-tertiary)",
+  background: "var(--palette-rose)",
+  background2: "var(--palette-white)",
+  activeColor: "var(--palette-rose)",
+  borderColor: "var(--palette-border)"
+} as const;
+</script>
