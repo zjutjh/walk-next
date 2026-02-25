473bcf83afb9e6cbb6c365b1c8d5baebd2220d5c
maojianhui <maojianhui2006@gmail.com>
2026-02-25 19:52:05 +0800

    style: color config export

--- apps/admin/src/configs/theme-vars.ts
+++ apps/admin/src/configs/theme-vars.ts
@@ -1,13 +1,7 @@
-export const PRIMARY_COLOR = "#F58888";
-export const SUCCESS_COLOR = "#49DF17";
-export const WARNING_COLOR = "#ADDF17";
-export const DANGER_COLOR = "#ED7461";
-export const INFO_COLOR = "#3A87FA";
-
 export const THEME_VAR_RECORD = {
-  primaryColor: PRIMARY_COLOR,
-  successColor: SUCCESS_COLOR,
-  warningColor: WARNING_COLOR,
-  dangerColor: DANGER_COLOR,
-  infoColor: INFO_COLOR
+  primaryColor: "#F58888",
+  successColor: "#49DF17",
+  warningColor: "#ADDF17",
+  dangerColor: "#ED7461",
+  infoColor: "#3A87FA"
 } as const;
