15e6b1b98afa0838dec92d39e7fafd938a6acc78
樱岛麻衣 <maojianhui2006@gmail.com>
2026-02-25 19:57:36 +0800

    feat: 颜色配置
    feat: 颜色配置

--- apps/admin/src/app.vue
+++ apps/admin/src/app.vue
@@ -1,3 +1,9 @@
 <template>
-  <router-view />
+  <van-config-provider :theme-vars="THEME_VAR_RECORD" theme-vars-scope="global">
+    <router-view />
+  </van-config-provider>
 </template>
+
+<script setup lang="ts">
+import { THEME_VAR_RECORD } from "@/configs/theme-vars";
+</script>
---
+++ apps/admin/src/configs/theme-vars.ts
@@ -0,0 +1,7 @@
+export const THEME_VAR_RECORD = {
+  primaryColor: "#F58888",
+  successColor: "#49DF17",
+  warningColor: "#ADDF17",
+  dangerColor: "#ED7461",
+  infoColor: "#3A87FA"
+} as const;
