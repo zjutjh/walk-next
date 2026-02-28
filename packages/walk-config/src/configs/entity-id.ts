2974ccd0bccc2a98309711b91c48c72a42bff8ee
DumbDaiDai <3234627960@qq.com>
2026-02-28 21:00:38 +0800

    feat: 添加新的内容

--- packages/walk-config/src/configs/entity-id.ts
+++ packages/walk-config/src/configs/entity-id.ts
@@ -8,23 +8,23 @@
  */
 
 import { uniq } from "lodash-es";
-import { ValueOf } from "type-fest";
+import { Simplify, ValueOf } from "type-fest";
 
 /**
  *
  *
- * 配置
+ * 配置（修改时按从上到下顺序）
  */
 
 /** 校区ID列表
  * @property {string} 校区ID */
 // cspell:disable
-export const WALK_CAMPUS_ID = ["pf", "mgs"] as const;
+export const WALK_CAMPUS_ID_LIST = ["pf", "mgs"] as const;
 // cspell:enable
 
 /** 校区ID-路线ID列表 映射表
- * @property {string[]} 路线ID列表 */
-export const WALK_ROUTE_ID_MAP = {
+ * 详细注释 @see */
+const ROUTE_ID_MAP = {
   // cspell:disable
   pf: ["pf-full", "pf-half"],
   mgs: ["mgs"]
@@ -32,8 +32,8 @@ export const WALK_ROUTE_ID_MAP = {
 } as const satisfies Record<WalkCampusId, string[]>;
 
 /** 路线ID-路径点ID列表 映射表
- * @property {string[]} 点位ID列表（从起点到终点 按行走顺序排列，点位可重复出现） */
-export const WALK_PATH_POINT_ID_MAP = {
+ * 详细注释 @see WALK_POINT_ID_MAP */
+const PATH_POINT_ID_MAP = {
   // cspell:disable
   "pf-full": ["pfxq", "jls", "blt", "cmq", "gzsgy", "pfs", "pfsy", "pfxq"],
   "pf-half": ["pfxq", "jls", "ljs", "pfs", "pfsy", "pfxq"],
@@ -49,40 +49,98 @@ export const WALK_PATH_POINT_ID_MAP = {
 
 /** 路线ID列表
  *
- * @see {WALK_ROUTE_ID_MAP} 数据源 */
-export const WALK_ROUTE_ID_LIST = Object.values(WALK_ROUTE_ID_MAP).flat();
+ * @see ROUTE_ID_MAP 数据源 */
+export const WALK_ROUTE_ID_LIST = Object.values(ROUTE_ID_MAP).flat();
 
 /** 点位ID列表
  *
- * @see {WALK_PATH_POINT_ID_MAP} 数据源 */
-export const WALK_POINT_ID_LIST = uniq(Object.values(WALK_PATH_POINT_ID_MAP).flat());
+ * @see PATH_POINT_ID_MAP 数据源 */
+export const WALK_POINT_ID_LIST = uniq(Object.values(PATH_POINT_ID_MAP).flat());
+
+/** 校区ID-点位ID列表 映射表
+ *
+ * 数据源 @see WALK_CAMPUS_ID_LIST
+ * 数据源 @see ROUTE_ID_MAP
+ * 数据源 @see PATH_POINT_ID_MAP
+ *
+ * 详细注释 @see WALK_CAMPUS_POINT_ID_MAP */
+const CAMPUS_POINT_ID_MAP = (() => {
+  const result: Partial<Record<WalkCampusId, WalkPointId[]>> = {};
+  // 遍历所有校区ID
+  for (const campusId of WALK_CAMPUS_ID_LIST) {
+    // 计算点位ID
+    result[campusId] = uniq(
+      ROUTE_ID_MAP[campusId].flatMap((routeId) => PATH_POINT_ID_MAP[routeId])
+    );
+  }
+  return result;
+})() as unknown as WalkCampusIdMap;
 
 /**
  *
  *
- * 类型
+ * 类型（无需修改，自动推导）
  */
-
-/** 使Record访问成员时对字符串键更宽容的
- * @description 不在Record的键中的字符串也被认为存在值，但这些非预期的Key对应的值可能是undefined而非ValueType
- */
-export type AllowAnyStringKey<RecordType extends Record<string, unknown>> = Record<
-  keyof RecordType,
-  ValueOf<RecordType>
-> &
-  Record<string, ValueOf<RecordType> | undefined>;
 
 /** 校区ID
  *
- * @see {WALK_CAMPUS_ID} 数据源 */
-export type WalkCampusId = (typeof WALK_CAMPUS_ID)[number];
+ * 数据源 @see WALK_CAMPUS_ID_LIST */
+export type WalkCampusId = (typeof WALK_CAMPUS_ID_LIST)[number];
 
 /** 路线ID
  *
- * @see {WALK_ROUTE_ID_MAP} 数据源 */
-export type WalkRouteId = ValueOf<typeof WALK_ROUTE_ID_MAP>[number];
+ * 数据源 @see ROUTE_ID_MAP */
+export type WalkRouteId = ValueOf<typeof ROUTE_ID_MAP>[number];
 
 /** 点位ID
  *
- * @see {WALK_PATH_POINT_ID_MAP} 数据源 */
-export type WalkPointId = ValueOf<typeof WALK_PATH_POINT_ID_MAP>[number];
+ * 数据源 @see PATH_POINT_ID_MAP */
+export type WalkPointId = ValueOf<typeof PATH_POINT_ID_MAP>[number];
+
+/** 校区ID-点位ID列表 映射表
+ *
+ * 数据源 @see ROUTE_ID_MAP
+ * 数据源 @see PATH_POINT_ID_MAP */
+export type WalkCampusIdMap = {
+  // 遍历所有 校区ID
+  [CampusId in WalkCampusId]: {
+    // 遍历 校区ID 对应的 路段ID列表，进而取得 路段ID 对应的 点位ID
+    [RouteId in (typeof ROUTE_ID_MAP)[CampusId][number]]: (typeof PATH_POINT_ID_MAP)[RouteId][number];
+  }[(typeof ROUTE_ID_MAP)[CampusId][number]];
+};
+
+/**
+ *
+ *
+ * 将映射表换种类型导出，允许用一般string类型作为Key访问，并确保类型安全
+ * @description 由于这里的值才是实际导出值，因此修改JSDoc注释需要在这里修改
+ */
+
+/** 使访问只读Record成员时对字符串Key更宽容
+ * @description 不在Record的Key中的字符串对应的值会被推断出可能是undefined
+ */
+export type AllowAnyStringKey<RecordType extends Readonly<Record<string, unknown>>> = Simplify<
+  Readonly<
+    Record<keyof RecordType, ValueOf<RecordType>> & Record<string, ValueOf<RecordType> | undefined>
+  >
+>;
+
+/** 校区ID-路线ID列表 映射表
+ * @property {string[]} 路线ID列表
+ *
+ * 修改 @see ROUTE_ID_MAP */
+export const WALK_ROUTE_ID_MAP = ROUTE_ID_MAP as AllowAnyStringKey<typeof ROUTE_ID_MAP>;
+
+/** 路线ID-路径点ID列表 映射表
+ * @property {string[]} 点位ID列表（从起点到终点 按行走顺序排列，点位可重复出现）
+ *
+ * 修改 @see PATH_POINT_ID_MAP */
+export const WALK_POINT_ID_MAP = PATH_POINT_ID_MAP as AllowAnyStringKey<typeof PATH_POINT_ID_MAP>;
+
+/** 校区ID-点位ID列表 映射表
+ * @description 每个校区的点位列表中没有重复
+ *
+ * 实现 @see CAMPUS_POINT_ID_MAP */
+export const WALK_CAMPUS_POINT_ID_MAP = CAMPUS_POINT_ID_MAP as AllowAnyStringKey<
+  typeof CAMPUS_POINT_ID_MAP
+>;
--- packages/walk-config/src/configs/entity-info.ts
+++ packages/walk-config/src/configs/entity-info.ts
@@ -6,7 +6,7 @@
  *
  */
 
-import { Component } from "vue";
+import { Simplify, ValueOf } from "type-fest";
 
 import {
   mgsMapUrl,
@@ -22,91 +22,86 @@ import { WalkCampusId, WalkPointId, WalkRouteId } from "./entity-id";
 /**
  *
  *
- * 配置
+ * 配置（修改时按从上到下顺序）
  */
 
 /** 校区配置
- * @property {string} name 校区名
- * @property {string} mapUrl 地图URL
- */
-export const WALK_CAMPUS_CONFIG = {
+ * 详细注释 @see WALK_CAMPUS_CONFIG */
+const CAMPUS_CONFIG = {
   pf: {
-    name: "屏峰",
+    text: "屏峰",
     mapUrl: pfMapUrl,
     pointLegendUrl: pfPointLegendUrl
   },
   mgs: {
-    name: "莫干山",
+    text: "莫干山",
     mapUrl: mgsMapUrl,
     pointLegendUrl: mgsPointLegendUrl
   }
 } as const satisfies Record<WalkCampusId, WalkCampusConfig>;
 
 /** 路线配置
- * @property {string} name 路线名
- */
-export const WALK_ROUTE_CONFIG = {
+ * 详细注释 @see WALK_ROUTE_CONFIG */
+const ROUTE_CONFIG = {
   "pf-full": {
-    name: "屏峰全程",
+    text: "屏峰全程",
     legendUrl: pfFullRouteLegendUrl,
     dataCardColor: "#fa2d2d"
   },
   "pf-half": {
-    name: "屏峰半程",
+    text: "屏峰半程",
     legendUrl: pfHalfRouteLegendUrl,
     dataCardColor: "#3a87fa"
   },
   mgs: {
-    name: "莫干山全程",
+    text: "莫干山全程",
     legendUrl: mgsRouteLegendUrl,
     dataCardColor: "#ff9c00"
   }
 } as const satisfies Record<WalkRouteId, WalkRouteConfig>;
 
 /** 点位配置
- * @property {string} name 点位名
- * @property {Component} hotSpot 点击热区
- */
-export const WALK_POINT_CONFIG = {
+ 详细注释 @see WALK_POINT_CONFIG */
+const POINT_CONFIG = {
   // cspell:disable
   pfxq: {
-    name: "屏峰校区"
+    text: "屏峰校区"
   },
   jls: {
-    name: "金莲寺"
+    text: "金莲寺"
   },
   blt: {
-    name: "白龙潭"
+    text: "白龙潭"
   },
   cmq: {
-    name: "慈母桥"
+    text: "慈母桥"
   },
   gzsgy: {
-    name: "古樟树公园"
+    text: "古樟树公园"
   },
   ljs: {
-    name: "老焦山"
+    text: "老焦山"
   },
   pfs: {
-    name: "屏峰山"
+    text: "屏峰山"
   },
   pfsy: {
-    name: "屏峰善院"
+    text: "屏峰善院"
   },
   mgsxq: {
-    name: "莫干山校区"
+    text: "莫干山校区"
   },
   zfgy: {
-    name: "兆丰公园"
+    text: "兆丰公园"
   },
   hbgy: {
-    name: "滑板公园"
+    text: "滑板公园"
   },
   tayg: {
-    name: "天安云谷"
+    text: "天安云谷"
   },
   dtx: {
-    name: "东苕溪"
+    text: "东苕溪"
   }
   // cspell:enable
 } as const satisfies Record<WalkPointId, WalkPointConfig>;
@@ -114,13 +109,13 @@ export const WALK_POINT_CONFIG = {
 /**
  *
  *
- * 类型
+ * 类型（无需修改，自动推导）
  */
 
 /** 校区配置 */
 export interface WalkCampusConfig {
   /** 校区的中文全名（不含“校区”二字） */
-  name: string;
+  text: string;
   /** 校区的地图的URL */
   mapUrl: string;
   /** 地图上点位的图例URL */
@@ -129,8 +124,8 @@ export interface WalkCampusConfig {
 
 /** 路线配置 */
 export interface WalkRouteConfig {
-  /** 路线的中文全名 */
-  name: string;
+  /** 路线的中文全名（不含“路线”二字） */
+  text: string;
   /** 地图上路线的图例URL */
   legendUrl: string;
   /** 数据仪表盘页数据总览的颜色（CSS值） */
@@ -140,7 +135,43 @@ export interface WalkRouteConfig {
 /** 点位配置 */
 export interface WalkPointConfig {
   /** 点位的中文全名 */
-  name: string;
-  /** 点位的地图点击热区 */
-  hotSpot?: Component; // TODO: 完成热区后将此字段改为必需
+  text: string;
+  /** 点位的地图点击热区HTML */
+  hotSpotHTML?: string; // TODO: 完成热区后将此字段改为必需
 }
+
+/**
+ *
+ *
+ * 将映射表换种类型导出，允许用一般string类型作为Key访问，并确保类型安全
+ * @description 由于这里的值才是实际导出值，因此修改JSDoc注释需要在这里修改
+ */
+
+/** 使访问只读Record成员时对字符串Key更宽容
+ * @description 不在Record的Key中的字符串对应的值会被推断出可能是undefined
+ */
+type AllowAnyStringKey<RecordType extends Readonly<Record<string, unknown>>> = Simplify<
+  Readonly<
+    Record<keyof RecordType, ValueOf<RecordType>> & Record<string, ValueOf<RecordType> | undefined>
+  >
+>;
+
+/** 校区配置
+ * @property {string} text 校区名（不含“校区”二字）
+ * @property {string} mapUrl 地图URL
+ *
+ * 修改 @see CAMPUS_CONFIG */
+export const WALK_CAMPUS_CONFIG = CAMPUS_CONFIG as AllowAnyStringKey<typeof CAMPUS_CONFIG>;
+
+/** 路线配置
+ * @property {string} text 路线名（不含“路线”二字）
+ *
+ * 修改 @see ROUTE_CONFIG */
+export const WALK_ROUTE_CONFIG = ROUTE_CONFIG as AllowAnyStringKey<typeof ROUTE_CONFIG>;
+
+/** 点位配置
+ * @property {string} text 点位名
+ * @property {Component} hotSpot 点击热区
+ *
+ * 修改 @see POINT_CONFIG */
+export const WALK_POINT_CONFIG = POINT_CONFIG as AllowAnyStringKey<typeof POINT_CONFIG>;
