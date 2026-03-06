527f00844053e6a284013e5bf6c6fa113cacd076
DumbDaiDai <3234627960@qq.com>
2026-03-06 20:54:22 +0800

    feat: 封装通用错误态组件

---
+++ apps/admin/src/components/error-empty/index.vue
@@ -0,0 +1,49 @@
+<!-- 错误态 -->
+<template>
+  <div v-if="props.error && !props.disabled" v-bind="$attrs" class="error-empty">
+    <van-empty class="error-empty__empty" :image="errorImageUrl" :image-size="props.imageSize">
+      <template #description>{{ description ?? props.error.message }}</template>
+    </van-empty>
+    <van-button
+      v-if="props.showRetry"
+      class="error-empty__retry"
+      round
+      type="primary"
+      @click="handleRetry"
+      >重试</van-button
+    >
+  </div>
+  <slot v-else />
+</template>
+
+<script setup lang="ts">
+import "./index.scss";
+
+import type { EmptyProps } from "vant";
+
+import errorImageUrl from "@/assets/error.png";
+
+export interface ErrorEmptyProps extends Pick<EmptyProps, "imageSize" | "description"> {
+  /** 错误 */
+  error: Error | null | undefined;
+  /** 忽略错误，不展示错误态 */
+  disabled?: boolean;
+  /** 是否显示重试按钮 */
+  showRetry?: boolean;
+}
+
+const props = withDefaults(defineProps<ErrorEmptyProps>(), {
+  imageSize: "1.6rem",
+  description: undefined,
+  disabled: false,
+  showRetry: true
+});
+
+const emit = defineEmits<{
+  retry: [];
+}>();
+
+const handleRetry = () => {
+  emit("retry");
+};
+</script>
