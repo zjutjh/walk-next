7f5c2a3d3873d39758c90e9d011beb312cbf6a1e
DumbDaiDai <3234627960@qq.com>
2026-05-17 09:03:34 +0800

    fix: 去除已废弃的prop

--- apps/admin/src/pages/walk-dashboard/index.vue
+++ apps/admin/src/pages/walk-dashboard/index.vue
@@ -1,10 +1,6 @@
 <!-- 数据仪表盘页 -->
 <template>
-  <default-layout
-    :class="styles.layout"
-    :title="`${CAMPUS_CONFIG[props.campusId]?.text}数据大盘`"
-    :scroll="false"
-  >
+  <default-layout :class="styles.layout" :title="`${CAMPUS_CONFIG[props.campusId]?.text}数据大盘`">
     <template #right>
       <van-icon name="search" size="22" @click="handleSearchClick" />
     </template>
