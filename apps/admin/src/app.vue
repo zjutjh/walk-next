2d274ebf96b677e808384f056454bd9ddd8c2a70
DumbDaiDai <3234627960@qq.com>
2026-07-10 13:37:08 +0800

    feat: 安装鼠标适配

--- apps/admin/src/app.vue
+++ apps/admin/src/app.vue
@@ -7,6 +7,8 @@
 </template>
 
 <script setup lang="ts">
+import "@vant/touch-emulator";
+
 import { ready } from "qr-scanner-wechat";
 import { showFailToast } from "vant";
 import { ref } from "vue";
