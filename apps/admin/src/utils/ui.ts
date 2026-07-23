e2cb994344ae2e57cf1de13d99a7e354ddf5e101
DumbDaiDai <3234627960@qq.com>
2026-07-23 10:56:39 +0800

    refactor: 使用更具逻辑一致性的方式编写UI缩放代码

--- apps/admin/src/utils/ui.ts
+++ apps/admin/src/utils/ui.ts
@@ -1,7 +1,7 @@
 import { round } from "lodash-es";
 
-import { getViewWidth } from "@/configs";
-import { BASE_ROOT_FONT_SIZE, DESIGN_WIDTH, MAX_REM_UNIT_PRECISION } from "@/constants";
+import { getUiScaleRatio } from "@/configs";
+import { BASE_ROOT_FONT_SIZE, MAX_REM_UNIT_PRECISION } from "@/constants";
 
 /**
  * 手动将 px 转换为 rem，需要动态计算元素尺寸才用，一般不用
@@ -18,6 +18,6 @@ export function pxToRem(px: number): `${number}rem` {
  * 计算出的尺寸不会随屏幕宽度变化有响应式关系
  */
 export function pxToSize(px: number): number {
-  // 无需round，由渲染引擎自行处理
-  return (px / DESIGN_WIDTH) * getViewWidth();
+  // 此处无需round，由渲染引擎自行处理精度
+  return px * getUiScaleRatio();
 }
