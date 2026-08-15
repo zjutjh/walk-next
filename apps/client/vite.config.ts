31e810735dd85ab7588e9816a090ed43e4930b3b
shy <236537615+Shy-06@users.noreply.github.com>
2026-08-15 16:55:24 +0800

    feat: 添加 vue-i18n 插件支持并更新类型定义

--- apps/client/vite.config.ts
+++ apps/client/vite.config.ts
@@ -1,10 +1,17 @@
+import VueI18nPlugin from "@intlify/unplugin-vue-i18n/vite";
 import vue from "@vitejs/plugin-vue";
 import path from "path";
 import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    VueI18nPlugin({
+      include: [path.resolve(__dirname, "./src/locales/**")],
+      treeShaking: true
+    })
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src")
