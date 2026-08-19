dd0d036c39501aadd1e0fbc0530912ec07c67f33
1inson <690335285@qq.com>
2026-08-19 16:22:17 +0800

    fix: 接入 client 错误边界

--- apps/client/src/app.vue
+++ apps/client/src/app.vue
@@ -1,3 +1,9 @@
 <template>
-  <error-boundary></error-boundary>
+  <error-boundary>
+    <h1>Init</h1>
+  </error-boundary>
 </template>
+
+<script setup lang="ts">
+import ErrorBoundary from "@/components/error-boundary/index.vue";
+</script>
