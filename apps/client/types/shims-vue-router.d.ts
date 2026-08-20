a7b21a9e9f14c661036fd02dd84886261fb7041c
1inson <690335285@qq.com>
2026-08-20 14:01:10 +0800

    feat:判断团队身份

--- apps/client/types/shims-vue-router.d.ts
+++ apps/client/types/shims-vue-router.d.ts
@@ -1,5 +1,8 @@
 import "vue-router";
 
+// To ensure it is treated as a module, add at least one `export` statement
+export {};
+
 declare module "vue-router" {
   interface RouteMeta {
     /** 中文页面名称 */
@@ -8,5 +11,7 @@ declare module "vue-router" {
     allowNoAuth?: boolean;
     /** 路由参数变化时强制重建组件 */
     recreateComponentByPath?: boolean;
+    /** 允许访问该路由的团队身份 */
+    allowedRoles?: UserRole[];
   }
 }
