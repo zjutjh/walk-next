4eef4c93de9b608f3ea303ac87557c46fa3cc8d9
1inson <690335285@qq.com>
2026-08-21 14:07:12 +0800

    feat:routerview加入app.vue&routerInstance加入main.ts

--- apps/client/src/main.ts
+++ apps/client/src/main.ts
@@ -5,10 +5,12 @@ import piniaPluginPersistedstate from "pinia-plugin-persistedstate";
 import { createApp } from "vue";
 
 import { useUserLocale } from "@/composables";
+import { routerInstance } from "@/configs";
 
 import App from "./app.vue";
 
 createApp(App)
+  .use(routerInstance)
   .use(createPinia().use(piniaPluginPersistedstate))
   .use(useUserLocale().getInstance())
   .mount("#app");
