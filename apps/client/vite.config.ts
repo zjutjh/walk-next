65eef354f125f5bb6f796f014107c91bcaf9db85
1inson <690335285@qq.com>
2026-08-21 16:16:40 +0800

    feat:vite.config.ts增加proxy和vant自动导入

--- apps/client/vite.config.ts
+++ apps/client/vite.config.ts
@@ -1,24 +1,53 @@
 import VueI18nPlugin from "@intlify/unplugin-vue-i18n/vite";
+import { VantResolver } from "@vant/auto-import-resolver";
 import vue from "@vitejs/plugin-vue";
 import path from "path";
+import AutoImport from "unplugin-auto-import/vite";
+import IconsResolver from "unplugin-icons/resolver";
 import Icons from "unplugin-icons/vite";
-import { defineConfig } from "vite";
+import Components from "unplugin-vue-components/vite";
+import { defineConfig, loadEnv } from "vite";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    VueI18nPlugin({
-      include: [path.resolve(__dirname, "./src/locales/**")],
-      treeShaking: true
-    }),
-    Icons({
-      compiler: "vue3"
-    })
-  ],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src")
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+
+  return {
+    base: env.VITE_BASE_PATH,
+    plugins: [
+      vue(),
+      VueI18nPlugin({
+        include: [path.resolve(__dirname, "./src/locales/**")],
+        treeShaking: true
+      }),
+      Icons({
+        compiler: "vue3"
+      }),
+      AutoImport({
+        resolvers: [VantResolver(), IconsResolver()],
+        dts: "types/auto-imports.d.ts"
+      }),
+      Components({
+        dirs: [],
+        resolvers: [VantResolver(), IconsResolver()],
+        dts: "types/components.d.ts"
+      })
+    ],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src")
+      }
+    },
+    server: {
+      proxy: {
+        "/api": {
+          target: env.VITE_HOST,
+          changeOrigin: true
+        }
+      }
+    },
+    build: {
+      target: "es2015"
     }
-  }
+  };
 });
