6fe08a815b2b72aee2cdd7052f8e213c1c85c29a
1inson <690335285@qq.com>
2026-08-24 04:44:54 +0800

    feat:未组队团队信息页

--- apps/client/src/pages/team-info/index.vue
+++ apps/client/src/pages/team-info/index.vue
@@ -1,3 +1,22 @@
 <template>
-  <main>TODO</main>
+  <main v-if="!isUserInfoReady" :class="styles.loadingPage">
+    <van-loading vertical>加载中</van-loading>
+  </main>
+
+  <unjoined-team-home v-else-if="isUnjoined" />
 </template>
+
+<script setup lang="ts">
+import { computed } from "vue";
+
+import { useClientUserData } from "@/composables";
+
+import UnjoinedTeamHome from "./components/unjoined-team/index.vue";
+import styles from "./index.module.scss";
+
+const { clientUserInfo } = useClientUserData();
+
+const isUserInfoReady = computed(() => Boolean(clientUserInfo.value));
+
+const isUnjoined = computed(() => clientUserInfo.value?.role === "unbind");
+</script>
