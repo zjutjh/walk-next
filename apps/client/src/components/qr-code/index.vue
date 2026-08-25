b44a0687aba4e50f542eb3500e7f2913a99e0fd8
Promise <i@xenlyra.cc>
2026-08-25 21:27:10 +0800

    feat: 二维码生成组件

---
+++ apps/client/src/components/qr-code/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <img v-if="qrDataUrl" :src="qrDataUrl" :class="props.class" alt="二维码" />
+</template>
+
+<script setup lang="ts">
+import { useQRCode } from "@vueuse/integrations";
+import { toRef } from "vue";
+
+const props = defineProps<{
+  /** 二维码内容 */
+  value: string;
+  /** 附加类名 */
+  class?: string;
+}>();
+
+/** 将二维码内容渲染为 data-url */
+const qrDataUrl = useQRCode(toRef(() => props.value));
+</script>
