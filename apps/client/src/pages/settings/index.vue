79a16a62e00b345651fcad2a06b9a8c166a5007b
1inson <690335285@qq.com>
2026-08-27 19:26:34 +0800

    feat:退出登录

--- apps/client/src/pages/settings/index.vue
+++ apps/client/src/pages/settings/index.vue
@@ -1,3 +1,39 @@
 <template>
-  <main>TODO</main>
+  <main :class="styles.page">
+    <van-cell-group inset>
+      <van-cell title="退出登录" clickable is-link @click="handleLogoutClick" />
+    </van-cell-group>
+  </main>
 </template>
+
+<script setup lang="ts">
+import { showConfirmDialog, showSuccessToast } from "vant";
+import { useRouter } from "vue-router";
+
+import { useClientUserData } from "@/composables";
+
+import styles from "./index.module.scss";
+
+const router = useRouter();
+const { resetClientUserData } = useClientUserData();
+
+const handleLogoutClick = async () => {
+  try {
+    await showConfirmDialog({
+      title: "退出登录",
+      message: "确认退出当前账号吗？"
+    });
+  } catch {
+    return;
+  }
+
+  resetClientUserData();
+
+  showSuccessToast({
+    message: "已退出登录",
+    position: "top"
+  });
+
+  await router.replace({ name: "login" });
+};
+</script>
