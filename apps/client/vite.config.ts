c6d267dc82388f14b611794654e72b5f7d0926cc
shy <236537615+Shy-06@users.noreply.github.com>
2026-08-27 11:54:42 +0800

    feat: 添加 vite-plugin-vue-devtools 插件以支持 Vue 开发工具

--- apps/client/vite.config.ts
+++ apps/client/vite.config.ts
@@ -8,6 +8,7 @@ import IconsResolver from "unplugin-icons/resolver";
 import Icons from "unplugin-icons/vite";
 import Components from "unplugin-vue-components/vite";
 import { defineConfig, loadEnv } from "vite";
+import vueDevTools from "vite-plugin-vue-devtools";
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
@@ -16,6 +17,7 @@ export default defineConfig(({ mode }) => {
   return {
     base: env.VITE_BASE_PATH,
     plugins: [
+      vueDevTools(),
       {
         enforce: "pre",
         ...mdx({
