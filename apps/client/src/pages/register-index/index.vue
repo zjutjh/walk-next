f4bd5a178db1906493129d9109cdd09ed22428f9
shy <236537615+Shy-06@users.noreply.github.com>
2026-08-30 18:03:42 +0800

    feat: 完善注册页面，添加装饰组件和相关样式

--- apps/client/src/pages/register-index/index.vue
+++ apps/client/src/pages/register-index/index.vue
@@ -1,3 +1,13 @@
 <template>
-  <div>TODO</div>
+  <div>
+    <decoration :src="proudImage" right="-75px" bottom="0" width="400px" max-width="75vw" />
+    <decoration :src="tomatoJamImage" top="120px" right="0" width="100px" max-width="25vw" mirror />
+    <decoration :src="tomatoJamImage" top="250px" left="0" width="150px" max-width="37.5vw" />
+  </div>
 </template>
+
+<script setup lang="ts">
+import proudImage from "@/assets/images/proud.png";
+import tomatoJamImage from "@/assets/images/tomato-jam.png";
+import Decoration from "@/components/decoration/index.vue";
+</script>
