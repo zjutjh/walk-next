e2d23f65fc8732429d64e33332a69621e1d54abb
shy <236537615+Shy-06@users.noreply.github.com>
2026-09-07 09:41:06 +0800

    fix: 修复二维码组件中国际化文本的引用方式

--- apps/client/src/components/qr-code/index.vue
+++ apps/client/src/components/qr-code/index.vue
@@ -1,13 +1,10 @@
 <template>
-  <img v-if="qrDataUrl" :src="qrDataUrl" :alt="t('二维码')" />
+  <img v-if="qrDataUrl" :src="qrDataUrl" :alt="$t('二维码')" />
 </template>
 
 <script setup lang="ts">
 import { useQRCode } from "@vueuse/integrations";
 import { toRef } from "vue";
-import { useI18n } from "vue-i18n";
-
-const { t } = useI18n();
 
 const props = defineProps<{
   /** 二维码内容 */
